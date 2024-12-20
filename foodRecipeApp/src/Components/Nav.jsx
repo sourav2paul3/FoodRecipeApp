24e105a81f26f4661f56ec729cf52a512e6164fe
sourav2paul3 <40515551+sourav2paul3@users.noreply.github.com>
2024-12-20 15:51:35 +0530

    Final Changes

--- foodRecipeApp/src/Components/Nav.jsx
+++ foodRecipeApp/src/Components/Nav.jsx
@@ -1,4 +1,4 @@
 import styles from "../css/nav.module.css";
 export function Nav() {
-  return <div className={styles.nav}>🍔 Food App</div>;
+  return <div className={styles.nav}>🍕 Find Your Recipe</div>;
 }
